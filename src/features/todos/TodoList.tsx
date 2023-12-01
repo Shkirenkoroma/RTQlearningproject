5e194f1550bb9443e6b5f7fafc0f17e675a96852
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-12-02 00:52:29 +0300

    feature/RTQ: added settings

--- src/features/todos/TodoList.tsx
+++ src/features/todos/TodoList.tsx
@@ -1,38 +1,85 @@
-import { faUpload } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { FC, useState } from 'react';
+import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons';
+import {
+  useGetTodosQuery,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+  useAddTodoMutation,
+} from '../api/apiSlice';
 
-const TodoList: FC = (): JSX.Element => {
+const TodoList = () => {
   const [newTodo, setNewTodo] = useState('');
+  const {
+    data: todos,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetTodosQuery();
 
-  const handleSubmit = (e: any): void => {
+  const [addTodo] = useAddTodoMutation();
+  const [updateTodo] = useUpdateTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
+
+  const handleSubmit = (e:any) => {
     e.preventDefault();
+    addTodo({ userId: 1, title: newTodo, completed: false });
     setNewTodo('');
   };
 
+  const newItemSection = (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="new-todo">Enter a new todo item</label>
+      <div className="new-todo">
+        <input
+          type="text"
+          id="new-todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter new todo"
+        />
+      </div>
+      <button className="submit">
+        <FontAwesomeIcon icon={faUpload} />
+      </button>
+    </form>
+  );
+
   let content;
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (isSuccess) {
+    content = todos.map((todo: any) => {
+      return (
+        <article key={todo.id}>
+          <div className="todo">
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              id={todo.id}
+              onChange={() =>
+                updateTodo({ ...todo, completed: !todo.completed })
+              }
+            />
+            <label htmlFor={todo.id}>{todo.title}</label>
+          </div>
+          <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </article>
+      );
+    });
+  } else if (isError) {
+    content = <p>{error}</p>;
+  }
 
   return (
     <main>
       <h1>Todo List</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="new-todo">Enter a new todo item</label>
-        <div className="new-todo">
-          <input
-            type="text"
-            id="new-todo"
-            value={newTodo}
-            onChange={(e) => setNewTodo(e.target.value)}
-            placeholder="Enter new todo"
-          />
-        </div>
-        <button className="submit">
-          <FontAwesomeIcon icon={faUpload} />
-        </button>
-      </form>
+      {newItemSection}
       {content}
     </main>
   );
 };
-
 export default TodoList;
